e0eb6a94d2c773f4f5ffcceebe906754c7d0e3ff
Kooreis <kooreis.official@gmail.com>
2023-12-08 02:17:05 -0600

    Importing necessary modules: os for interacting with the operating system, shutil for high-level file operations, and datetime for working with dates and times.

--- Simple_Backup_Text_File_Generator.py
+++ Simple_Backup_Text_File_Generator.py
@@ -1,19 +1,3 @@
 import os
 import shutil
 import datetime
-
-def main():
-    src_folder = input("Enter source folder: ")
-    dest_folder = input("Enter destination folder: ")
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
-    dest_folder = os.path.join(dest_folder, datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
-    os.makedirs(dest_folder)
-    src_files = os.listdir(src_folder)
-    for file_name in src_files:
-        full_file_name = os.path.join(src_folder, file_name)
-        if os.path.isfile(full_file_name):
-            shutil.copy(full_file_name, dest_folder)
-
-if __name__ == "__main__":
-    main()
